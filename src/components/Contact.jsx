84fde1338f1b53d2dc64f73154d67be558e9a03b
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-01-01 19:59:32 -0300

    #3D-16 adding email function on contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
+
 const Contact = () => {
 
   const formRef = useState();
@@ -17,9 +19,41 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send(
+      'web-portifolio', 
+      'template_ws3288l',
+      {
+        from_name: form.name,
+        to_name: 'Emanuel',
+        from_email: form.email,
+        to_email: 'emanuelconte.dev@gmail.com',
+        message: form.message,
+      },
+      'DdSRLJPcepSEk3IhA'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you. I will get back to you as soon as possible.');
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+      console.log(error);
+      alert('Something went wrong.')
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
